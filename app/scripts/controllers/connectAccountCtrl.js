51cce3747f7d2047f1167e31761363453acda833
DmitryShamak <hellkitedmitry@mail.ru>
2016-03-17 15:02:33 +0300

    Connect account providers

--- app/scripts/controllers/connectAccountCtrl.js
+++ app/scripts/controllers/connectAccountCtrl.js
@@ -1,7 +1,22 @@
 angular.module("app")
-	.controller("ConnectAccountCtrl", function($rootScope, $scope) {
+	.controller("ConnectAccountCtrl", function($rootScope, $scope, api) {
 		$scope.page = {
 			noAccount: false
+		};
+
+		$scope.page.providers = [{
+			type: 'google',
+			available: true
+		}, {
+			type: 'linkedin'
+		}, {
+			type: 'instagram'
+		}, {
+			type: 'vk'
+		}];
+
+		$scope.connectToProvider = function(provider) {
+			api.auth(provider);
 		};
 
 		$scope.init = function() {
--- app/scripts/factories/api-service.js
+++ app/scripts/factories/api-service.js
@@ -3,24 +3,29 @@ angular.module("app")
     //    // Don't strip trailing slashes from calculated URLs
     //    $resourceProvider.defaults.stripTrailingSlashes = false;
     //}])
-    .factory("api", function($resource) {
+    .factory("api", function($resource, $window) {
         var api = {};
+        api.serverUrl = "http://localhost:3337";
         /*
         * User [add, get, find, update]
         * scoreboard [add, get, update, delete]
         * */
-        api.login = $resource("/api/login");
-        api.user = $resource("/api/user", null, {
+        api.login = $resource(api.serverUrl + "/api/login");
+        api.auth = function(provider) {
+            var path = api.serverUrl + "/auth/" + provider.type;
+            $window.location.href = path;
+        };
+        api.user = $resource(api.serverUrl + "/api/user", null, {
             'update': { method:'PUT' }
         });
-        api.authenticate = $resource("/api/authenticate");
-        api.scoreboard = $resource("/api/scoreboard/:key", null, {
+        api.authenticate = $resource(api.serverUrl + "/api/authenticate");
+        api.scoreboard = $resource(api.serverUrl + "/api/scoreboard/:key", null, {
             'update': { method:'PUT' }
         });
-        api.source = $resource("/api/source", null, {
+        api.source = $resource(api.serverUrl + "/api/source", null, {
             'update': { method:'PUT' }
         });
-        api.search = $resource("/api/search");
+        api.search = $resource(api.serverUrl + "/api/search");
 
         return api;
     });
