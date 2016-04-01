5ef244683e7fe257352b2a7e5806ea3add643ef0
DmitryShamak <hellkitedmitry@mail.ru>
2016-04-01 15:26:41 +0300

    Add feed template

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -5,10 +5,24 @@ angular.module("app")
 			busy: true
 		};
 
+		$scope.faker = function() {
+			var feeds = _.filter($scope.user.plugins, {category: "scrappers"});
+			$scope.feeds = feeds;
+		};
+
+		$scope.toggleFeed = function(feed) {
+			feed.active = !feed.active;
+		};
+
 		$scope.init = function() {
 			$scope.page.busy = false;
 			$scope.page.offline = false;
 		};
 
+		$scope.$watch("user", function() {
+			if($scope.user) {
+				$scope.faker();
+			}
+		});
 		$scope.init();
 	});
