3569a27f356a3dbbb0186a7340f7b4ba8ec20981
DmitryShamak <hellkitedmitry@mail.ru>
2016-04-29 17:29:07 +0300

    Updated styles

---
+++ app/scripts/constants/notificationParams.js
@@ -0,0 +1,6 @@
+angular.module('app')
+    .constant('notificationParams', {
+        icon: {
+            info: "https://cdn2.iconfinder.com/data/icons/free-basic-icon-set-2/300/19-256.png"
+        }
+    });
--- app/scripts/controllers/NotificationCtrl.js
+++ app/scripts/controllers/NotificationCtrl.js
@@ -1,7 +1,49 @@
 angular.module("app")
-	.controller("NotificationCtrl", function($rootScope, $scope) {
+	.controller("NotificationCtrl", function($rootScope, $scope, notificationParams) {
 		$scope.toggleNotificationsList = function() {
+			if(!window.Notification) {
+				return $scope.notifications.unvailable = true;
+			}
+
+			if (Notification.permission != "granted") {
+				return Notification.requestPermission(function (status) {
+					if (Notification.permission !== status) {
+						Notification.permission = status;
+						$scope.notifications.offline = false;
+					} else {
+						return $scope.notifications.offline = true;
+					}
+
+					$scope.notifications.active = !$scope.notifications.active;
+				});
+			}
+
 			$scope.notifications.active = !$scope.notifications.active;
+		};
+
+		$scope.addNotification = function(title, options, delay) {
+			if (!window.Notification || Notification.permission != "granted") {
+				return $scope.notifications.offline = true;
+			}
+
+			if(!$scope.notifications.list) {
+				$scope.notifications.list = [];
+			}
+
+			var notification = new Notification(title, options);
+
+			//todo: show delayed notifications with promise chain
+			$scope.notifications.list.push({
+				tag: options.tag,
+				title: title,
+				body: options.body,
+				data: options.date || moment().toDate(),
+				notification: notification
+			});
+		};
+
+		$scope.closeNotificationsList = function() {
+			$scope.notifications.active = false;
 		};
 
 		var init = function() {
@@ -9,8 +51,11 @@ angular.module("app")
 				$scope.notifications = $rootScope.notifications = {};
 			}
 
+			if(!window.Notification) {
+				return $scope.notifications.unvailable = true;
+			}
+
 			$scope.notifications.busy = false;
-			$scope.notifications.offline = false;
 
 			$scope.notifications.link = {
 				title: "Notifications",
@@ -20,6 +65,14 @@ angular.module("app")
 				action: $scope.toggleNotificationsList,
 				value: 0
 			};
+
+			//todo: get user notifications
+			$scope.addNotification("Welcome", {
+				body: "Hi, there!",
+				data: 'I like peas.',
+				icon: notificationParams.icon.info,
+				tag: "info"
+			});
 		};
 
 		$scope.$on('userConnected', function (event, data) {
@@ -30,7 +83,8 @@ angular.module("app")
 			if($($ev.target).closest(".notifications-wrapper").length) {
 				return;
 			}
-			$scope.notifications.active = false;
+			$scope.closeNotificationsList();
+
 			$scope.apply($scope);
 		});
 	});
