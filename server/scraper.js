562a0461f20d70bf0000dd334e21dcf1a29de9ee
DmitryShamak <hellkitedmitry@mail.ru>
2016-05-24 17:06:10 +0300

    add adme portal

--- server/scraper.js
+++ server/scraper.js
@@ -5,17 +5,21 @@ var scraper = {
 };
 scraper.providers.onliner = require("./scrappers/onliner");
 scraper.providers['tut.by'] = require("./scrappers/tut.by");
+scraper.providers.belaruspartisan = require("./scrappers/belaruspartisan");
 ////
 scraper.providers.habrahabr = require("./scrappers/habrahabr");
-//////scraper.providers["eax.me"] = require("./scrappers/eax.me");
-////
+////////scraper.providers["eax.me"] = require("./scrappers/eax.me");
+
+
+scraper.providers.adme = require("./scrappers/adme");
+
 scraper.providers.stopgame = require("./scrappers/stopgame");
 scraper.providers["4game"] = require("./scrappers/4game");
-//scraper.providers.gog = require("./scrappers/gog");
+////scraper.providers.gog = require("./scrappers/gog");
 scraper.providers.buka = require("./scrappers/buka");
-//
-//scraper.providers.oz = require("./scrappers/oz");
-//
+////
+////scraper.providers.oz = require("./scrappers/oz");
+////
 scraper.providers["kg-portal"] = require("./scrappers/kg-portal");
 
 module.exports = scraper;
